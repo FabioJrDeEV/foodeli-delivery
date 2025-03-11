39e48317183bcb1556f191fa5b5d154617f642bd
FabioJrDeEV <fj453578@gmail.com>
2025-03-11 17:13:11 -0300

    feat: initial section

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import HomePage from "./page/home_page/HomePage";
+
+const App = () => {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/page/home_page/HomePage.jsx
@@ -0,0 +1,128 @@
+import Logo from "../../assets/img/logo.png";
+import Search from "../../assets/img/search.png";
+import Bolsa from "../../assets/img/bolsa.png";
+import Login from "../../assets/img/login.png";
+import Button from "../../assets/img/button.png";
+import Font from "../../assets/img/font.png";
+import Prato from "../../assets/img/prato.png";
+import "./HomePage.css";
+
+const HomePage = () => {
+  return (
+    <div className="w-100 min-vh-100">
+      <nav className="navbar navbar-expand-lg p-3">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <img src={Logo} alt="Logo" width="152px" height="45px" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse flex-grow-0"
+            id="navbarSupportedContent"
+          >
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 gap-lg-4">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Why Foodeli?
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Menu
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="d-none d-lg-flex gap-lg-4 align-items-center">
+            <div>
+              <button className="border-0 bg-transparent p-1">
+                <img src={Search} alt="Search" width="22px" height="22px" />
+              </button>
+            </div>
+            <div>
+              <button className="border-0 bg-transparent p-1">
+                <img src={Bolsa} alt="Carrinho" width="25px" height="24px" />
+              </button>
+            </div>
+            <div>
+              <button className="border-0 bg-transparent p-1">
+                <img
+                  src={Login}
+                  alt="Login Button"
+                  width="120px"
+                  height="49px"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+      <section className="w-100 min-vh-100">
+        <div className="container mt-5 mt-md-5 mt-lg-5">
+          <div className="row">
+            <div className="col-12 col-md-6 col-lg-6">
+              <button className="border-0 bg-transparent">
+                <img
+                  src={Button}
+                  alt="Button"
+                  width="204px"
+                  height="52px"
+                  className="mb-5"
+                />
+              </button>
+              <img
+                src={Font}
+                alt="Fonte texto"
+                className="mt-4 img-fluid order-1 order-md-2"
+              />
+              <p
+                className="mt-5"
+                style={{
+                  fontSize: "18px",
+                  fontWeight: "500",
+                  lineHeight: "30px",
+                  maxWidth: "450px",
+                }}
+              >
+                Our job is to filling your tummy with delicious food and with
+                fast and free delivery
+              </p>
+            </div>
+            <div className="col-12 col-md-6 col-lg-6 mt-3 mt-md-5 mt-lg-0 order-2 order-md-1">
+              <img
+                src={Prato}
+                alt="Prato imagem"
+                width="100%"
+                height="auto"
+                className="img-fluid"
+              />
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default HomePage;
